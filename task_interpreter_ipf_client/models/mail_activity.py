1b94350c8bc30f3df53f324c0d47f71c6bc18d77
Artur Chilangwa <artur.chilangwa@vertel.se>
2021-01-28 16:58:34 +0100

    task_interpreter_ipf_client and task_interpreter_ipf_server: Rename
    Also defined languages comes from server

---
+++ task_interpreter_ipf_client/models/mail_activity.py
@@ -0,0 +1,47 @@
+# -*- coding: UTF-8 -*-
+
+################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2019 N-Development (<https://n-development.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+from odoo import api, models
+
+
+class MailActivity(models.Model):
+    _inherit = "mail.activity"
+
+    @api.model
+    def preprocessing_activity_data(self,mail_activity):
+        return {
+            'tolkbokning': {
+                'distanstolkTypId': mail_activity.location_type,
+                'fromDatumTid': str(mail_activity.time_start),
+                'tomDatumTid': str(mail_activity.time_end),
+                'tolksprakId': mail_activity.interpreter_language,
+                'tolkkonId': mail_activity.interpreter_gender_preference,
+                'bestallandeKAnr': mail_activity.department_id and mail_activity.department_id.ka_ref or None,
+                'adress': {
+                    'adress': mail_activity.street,
+                    'gatuadress': mail_activity.street2,
+                    'postnr': mail_activity.zip,
+                    'ort': mail_activity.city,
+                    'adressat': 'ipsum',
+                },
+            },
+        }
---
+++ task_interpreter_ipf_client/models/res_config_settings.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from odoo import api, fields, models, tools, SUPERUSER_ID  # noqa:F401
+
+_logger = logging.getLogger(__name__)
+
+
+class IpfConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    ipf_server_url = fields.Char(string='Server URL',
+                                 config_parameter='api_ipf.server_url')
+    ipf_client_secret = fields.Char(string='Client Secret',
+                                    config_parameter='api_ipf.client_secret')
+    ipf_client_id = fields.Char(string='Client ID',
+                                config_parameter='api_ipf.client_id')
+    ipf_environment = fields.Selection(selection=[('U1', 'U1'),
+                                                  ('I1', 'I1'),
+                                                  ('T1', 'IT'),
+                                                  ('T2', 'T2'),
+                                                  ('PROD', 'PROD'), ],
+                                       string='Environment',
+                                       default='U1',
+                                       required=True,
+                                       config_parameter='api_ipf.environment')
+
+    @api.multi
+    def update_languages(self):
+        _logger.warn('NILS2')
+        self.env['ipf.interpreter.client'].populate_res_intepreter_language()
