afc7417c9a7978089f5e979cf47f3cc534f0c7af
Nils Nyman-Waara <nils.nyman-waara@vertel.se>
2021-02-02 15:19:25 +0100

    HOTFIX-Captial-letters-environment

--- task_interperator_ipf_client/models/res_config_settings.py
+++ task_interperator_ipf_client/models/res_config_settings.py
@@ -12,12 +12,12 @@ class IpfConfigSettings(models.TransientModel):
                                     config_parameter='api_ipf.client_secret')
     ipf_client_id = fields.Char(string='Client ID',
                                 config_parameter='api_ipf.client_id')
-    ipf_environment = fields.Selection(selection=[('u1', 'U1'),
-                                                  ('i1', 'I1'),
-                                                  ('t1', 'IT'),
-                                                  ('t2', 'T2'),
-                                                  ('prod', 'PROD'), ],
+    ipf_environment = fields.Selection(selection=[('U1', 'U1'),
+                                                  ('I1', 'I1'),
+                                                  ('T1', 'IT'),
+                                                  ('T2', 'T2'),
+                                                  ('PROD', 'PROD'), ],
                                        string='Environment',
-                                       default='u1',
+                                       default='U1',
                                        required=True,
                                        config_parameter='api_ipf.environment')
