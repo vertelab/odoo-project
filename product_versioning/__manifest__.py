5b41792ccc2d51185f9deb30145b843abdb53d74
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-04-11 10:00:34 +0200

    Updated version number

--- product_versioning/__manifest__.py
+++ product_versioning/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Product Versioning',
-    'version': '1.1',
+    'version': '13.0.1.1.0',
     'website': 'https://www.vertelab.com',
     'category': 'Operations/Project',
     'sequence': 10,
