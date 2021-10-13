f55af91fb7d97c84b51f37676979c476d947a7d3
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-10-13 19:59:38 +0530

    [ADD] Added new module project_timesheet_activity_begin_end to show Begin and End hours on project timehseet.

---
+++ project_timesheet_activity_begin_end/README.rst
@@ -0,0 +1,27 @@
+README
+======
+
+This module adds begin and end hours in project task timehseets.
+
+The module is maintained from this repo: https://github.com/vertelab/odoo-project/tree/14.0
+
+Download
+--------
+
+This python module is hosted on github repository branch. Can be downloaded by running this command:
+
+git clone https://github.com/vertelab/odoo-project -b 14.0
+
+Configuration
+------------
+
+
+
+Dependencies
+------------
+
+
+
+Version History
+---------------
+
---
+++ project_timesheet_activity_begin_end/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Project Task Timesheet Activities - Begin/End Hours',
+    'version': '14.0.1.0.1',
+    'author': 'Verified Email Europe AB',
+    'website': 'https://verified-email.com/',
+    'license': 'AGPL-3',
+    'category': 'Tools',
+    'depends': [
+        'hr_timesheet_activity_begin_end' #https://github.com/OCA/timesheet/tree/14.0/hr_timesheet_activity_begin_end
+    ],
+    'data': {
+        'views/project_views.xml'
+    },
+    'application': False,
+    'installable': True,
+}
