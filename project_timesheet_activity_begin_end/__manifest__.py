83d7e4034d4fef17dc6625ac1157ebf20fceb7fa
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-11-19 12:43:30 +0100

    Update __manifest__.py

--- project_timesheet_activity_begin_end/__manifest__.py
+++ project_timesheet_activity_begin_end/__manifest__.py
@@ -6,6 +6,9 @@
     'website': 'https://vertel.se/',
     'license': 'AGPL-3',
     'category': 'Tools',
+    'description': '''
+    Here is video for that: https://www.loom.com/share/58f0c25d1aed4f4b8b41dcd6ce5ab4b8 \n 
+    ''', 
     'depends': [
         'hr_timesheet_activity_begin_end' #https://github.com/OCA/timesheet/tree/14.0/hr_timesheet_activity_begin_end
     ],
