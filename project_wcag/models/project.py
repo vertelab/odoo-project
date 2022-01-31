d29cce8f62d0a390c5108459fec9652ced4611a9
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2022-01-31 16:00:35 +0000

    Tog bort wcag.state och la till ett nytt fält

--- project_wcag/models/project.py
+++ project_wcag/models/project.py
@@ -7,9 +7,9 @@ from dateutil.relativedelta import relativedelta
 
 class Project(models.Model):
     _inherit = 'project.project'
-    is_wcag = fields.Boolean(default=False)
-    wcag_state_ids = fields.Many2many(comodel_name='wcag.state', string="Wcags", help="Is a list of wcag rules for tasks in this project") 
-    # ~ @api.models
+    is_wcag = fields.Boolean(default=False, string="Wcag Project")
+    wcag_rule_ids = fields.Many2many(comodel_name = "wcag.rule", relation ="wcag_rule_project", string = "Wcag Rules", help = "The wcag rules we are measuring each object against")
+# ~ @api.models
     # ~ def add_wcag_task_based_on_state(self):
         # ~ for task in project_tasks:
 
@@ -17,9 +17,8 @@ class Project(models.Model):
         for record in self:
             tasks = self.env['project.task'].search([('project_id','=',record.id)])
             for task in tasks:
-                for state in record.wcag_state_ids:
-                    for rule in state.wcag_rule_ids:
-                        if not self.env['project.task.wcag'].search([('wcag_id','=',rule.id),('task_id','=',task.id)]):
-                            self.env['project.task.wcag'].create({'wcag_id':rule.id,'task_id':task.id})
+               for rule in record.wcag_rule_ids:
+                   if not self.env['project.task.wcag'].search([('wcag_id','=',rule.id),('task_id','=',task.id)]):
+                       self.env['project.task.wcag'].create({'wcag_id':rule.id,'task_id':task.id})
                     
                 
--- project_wcag/models/wcag.py
+++ project_wcag/models/wcag.py
@@ -11,23 +11,15 @@ class WcagRule(models.Model):
     name = fields.Char()
     w3c_no = fields.Char()
     description = fields.Char()
-    wcag_state_ids = fields.Many2many(comodel_name = "wcag.state", relation ="wcag_rule_wcag_state", string = "Wcag States", help = "The states which this rule belongs to")
+    project_ids = fields.Many2many(comodel_name = "project.project", relation ="wcag_rule_project", string = "Projects", help = "The projects which this rule belongs to")
     task_wcag_ids = fields.One2many(comodel_name='project.task.wcag', string="Wcags", help="Is a list of wcag criteria for this task/object", inverse_name='wcag_id') 
-    
-#Gonna be a subset of rules for a project
-class WcagState(models.Model):
-    _name = 'wcag.state'
-    name = fields.Char()
-    wcag_rule_ids = fields.Many2many(comodel_name = "wcag.rule", relation ="wcag_rule_wcag_state", string = "Wcag Rules", help = "The wcag rules we are measuring each object against")
-    project_id = fields.Many2many(comodel_name = "project.project", relation ="wcag_state_project_project", string = "Project", inverse_name='wcag_state_ids')
-    
     
 class ProjectTaskWcag(models.Model):
     _name = 'project.task.wcag'
     wcag_id = fields.Many2one(comodel_name = "wcag.rule", string = "Wcag Rule")
     task_id = fields.Many2one(comodel_name = "project.task",string = "Project Task")
     wcag_state = fields.Selection([ ('not_reviewed', 'Not reviewed'),('done', 'Done'),('partially_ok', 'Partially Ok'),('not_approved ', 'Not approved'),('not_relevant', 'Not Relevant'),],'State', default='not_reviewed')
-
+    notes = fields.Char(String = "Notes")
 
 
     
