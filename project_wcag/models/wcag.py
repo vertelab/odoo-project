1c7d91a51dece60b58a1d0b5c92ae781af66072e
Jakob Krabbe <jakob@syscare.se>
2022-02-03 08:27:22 +0000

    wcag: fixing with user details

--- project_wcag/models/wcag.py
+++ project_wcag/models/wcag.py
@@ -7,8 +7,9 @@ from dateutil.relativedelta import relativedelta
 #Gonna be a list of rules based on a excel spread sheet
 class WcagRule(models.Model):
     _name = 'wcag.rule'
-    
-    name = fields.Char()
+    _rec_name = 'wcag_name'
+    # ~ wcag_name = fields.Char()
+    wcag_name = fields.Char()
     w3c_no = fields.Char()
     description = fields.Char()
     project_ids = fields.Many2many(comodel_name = "project.project", relation ="wcag_rule_project", string = "Projects", help = "The projects which this rule belongs to")
@@ -18,7 +19,7 @@ class ProjectTaskWcag(models.Model):
     _name = 'project.task.wcag'
     wcag_id = fields.Many2one(comodel_name = "wcag.rule", string = "Wcag Rule")
     task_id = fields.Many2one(comodel_name = "project.task",string = "Project Task")
-    wcag_state = fields.Selection([ ('done', 'OK'), ('partially_ok', 'Partial'),('not_approved ', 'Failed'),('not_relevant', 'Not relevant'),('not_reviewed', '*blank*')],'State', default='partially_ok')
+    wcag_state = fields.Selection([ ('done', 'OK'), ('partially_ok', 'Partial'),('not_approved ', 'Failed'),('not_relevant', 'Not relevant'),('not_reviewed', '*blank*')],'State', default='not_relevant')
     notes = fields.Char(String = "Notes")
 
 
