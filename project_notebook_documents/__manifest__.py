0da9a3c33570bb7363171784792f7dbee8171d39
Han Wong <han.wong@vertel.se>
2022-02-11 08:49:23 +0000

    [ADD] added notebook tab Documents showing message_main_attachment_id in project.edit_project view

---
+++ project_notebook_documents/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2021 Vertel AB (<robin.calvin@vertel.se>)
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+#
+# https://www.odoo.com/documentation/14.0/reference/module.html
+#
+{
+    'name': 'Project Notebook Documents',
+    'version': '14.0.0.0',
+    'summary': '',
+    'category': 'Administration', 
+    'description': """ """,
+    'author': 'Vertel AB <han.wong@vertel.se>', 'Vertel AB <kriss-ellen.svensson@vertel.se>'
+    'website': 'https://vertel.se/apps/',
+    'license': 'AGPL-3',
+    'depends': ['project'],
+    'data': ['views/project_view.xml'],
+    'demo': [],
+    'application': False,
+    'installable': True,    
+    'auto_install': False,
+}
