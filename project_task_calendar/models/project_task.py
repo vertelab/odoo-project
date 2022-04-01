28f695e55eeb62110508a335f6ce81383580edcc
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2022-04-01 14:18:50 +0000

    Fixed the attende removal from the calander event after we remove an assigned person from a task

--- project_task_calendar/models/project_task.py
+++ project_task_calendar/models/project_task.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
-
+import logging
+_logger = logging.getLogger(__name__)
 
 class ProjectTask(models.Model):
     _inherit = 'project.task'
@@ -11,6 +12,12 @@ class ProjectTask(models.Model):
     # Writes a new post in the calendar that corresponds with the project task
     def create_event(self):
         for rec in self:
+            _logger.warning("create_event")
+            _logger.warning("create_event")
+            _logger.warning("create_event")
+            _logger.warning("create_event")
+            _logger.warning("create_event")
+            _logger.warning(f"rec.user_id:{rec.user_id}")
             arranger_id = rec.user_id.partner_id.id
             vals = {
                 'name': rec.name,
@@ -25,9 +32,20 @@ class ProjectTask(models.Model):
             if rec.calendar_event_id and rec.user_id:
                 rec.calendar_event_id.with_context(no_mail_to_attendees=True).write(vals)
             elif rec.calendar_event_id and not rec.user_id:
+                # ~ rec.calendar_event_id.unlink()
+                _logger.warning("create_event IF ELSE")
+                _logger.warning("create_event IF ELSE")
+                _logger.warning("create_event IF ELSE")
+                _logger.warning("create_event IF ELSE")
+                _logger.warning("create_event IF ELSE")
+                _logger.warning("create_event IF ELSE")
+                _logger.warning("create_event IF ELSE")
+                _logger.warning("create_event IF ELSE")
+                _logger.warning("create_event IF ELSE")
+                _logger.warning("create_event IF ELSE")
                 rec.calendar_event_id.write({
                     'user_id': False,
-                    'partner_id': False
+                    'partner_ids': [(5, 0, 0)],
                 })
             else:
                 rec.calendar_event_id = self.env['calendar.event'].with_context(no_mail_to_attendees=True).create(vals)
@@ -35,7 +53,9 @@ class ProjectTask(models.Model):
     # Calls create_event when the deadline or the assigned user on the project task is changed
     def write(self, values):
         res = super(ProjectTask, self).write(values)
-        if values.get('date_deadline') or values.get('user_id'):
+        _logger.warning(f"write values{values}")
+        # ~ if values.get('date_deadline') or values.get('user_id'):
+        if 'date_deadline' in values or 'user_id' in values:
             self.create_event()
         return res
     
