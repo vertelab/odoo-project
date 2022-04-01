49ae669f529c79a70e4e1b8bdfeb9ebb048db60a
iammiracle <miracleaayodele@gmail.com>
2022-04-01 10:26:03 +0100

    project task

--- project_task_calendar/models/project_task.py
+++ project_task_calendar/models/project_task.py
@@ -6,7 +6,7 @@ from odoo import models, fields, api, _
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    calendar_event_id = fields.Many2one('calendar.event')
+    calendar_event_id = fields.Many2one('calendar.event', readonly=True)
 
     # Writes a new post in the calendar that corresponds with the project task
     def create_event(self):
@@ -22,15 +22,20 @@ class ProjectTask(models.Model):
                 'user_id': rec.user_id.id,
                 'partner_ids': [(6, 0, [arranger_id])]
             }
-            if rec.calendar_event_id:
+            if rec.calendar_event_id and rec.user_id:
                 rec.calendar_event_id.with_context(no_mail_to_attendees=True).write(vals)
+            elif rec.calendar_event_id and not rec.user_id:
+                rec.calendar_event_id.write({
+                    'user_id': False,
+                    'partner_id': False
+                })
             else:
                 rec.calendar_event_id = self.env['calendar.event'].with_context(no_mail_to_attendees=True).create(vals)
 
     # Calls create_event when the deadline or the assigned user on the project task is changed
     def write(self, values):
         res = super(ProjectTask, self).write(values)
-        if (values.get('date_deadline') or values.get('user_id')) and values.get('calendar_event_id'):
+        if values.get('date_deadline') or values.get('user_id'):
             self.create_event()
         return res
     
@@ -39,3 +44,9 @@ class ProjectTask(models.Model):
         if self.calendar_event_id:
             self.calendar_event_id.unlink()
         res = super(ProjectTask, self).unlink()
+
+
+class CalendarEvent(models.Model):
+    _inherit = 'calendar.event'
+
+    partner_id = fields.Many2one('res.partner', string='Responsible Contact')
