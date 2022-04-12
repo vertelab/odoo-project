40d27910a1f7deef51453ddbd96af4677cd6aa27
Emanuel Bergsten <emanuel.bergsten@vertel.se>
2022-04-12 12:06:19 +0000

    Added 'mixed' option

--- project_wcag/models/wcag.py
+++ project_wcag/models/wcag.py
@@ -12,6 +12,7 @@ from lxml import html
 
 
 ZERO_WIDTH_CHAR = '\u200b'
+VALIDATION_SELECTIONS = [('auto', 'Automatic'), ('mixed', 'Mixed'), ('manual', 'Manual')]
 
 #Gonna be a list of rules based on a excel spread sheet
 class WcagRule(models.Model):
@@ -22,26 +23,26 @@ class WcagRule(models.Model):
     w3c_no = fields.Char()
     conformance_level = fields.Char()
     description = fields.Char()
-    validate_method = fields.Selection([('auto', 'Automatic'),('manual', 'Manual')],'Validate Method', default='manual')
-    
+    validate_method = fields.Selection(VALIDATION_SELECTIONS,'Validate Method', default='manual')
+
     @api.model
     def create(self, vals):
-        display_wcag_name = vals.get('w3c_no') or "" + " " + vals.get('conformance_level') or "" + " " +  vals.get('description') or "" 
+        display_wcag_name = vals.get('w3c_no') or "" + " " + vals.get('conformance_level') or "" + " " +  vals.get('description') or ""
         vals['display_wcag_name'] = display_wcag_name
         res = super(WcagRule, self).create(vals)
          # ~ if res.self.task_id:
             # ~ res.task_project_id = res.task_id.project_id
         return res
-        
+
     def write(self, values):
         if values.get('description') or values.get('w3c_no') or values.get('conformance_level'):
             w3c_no = values.get('w3c_no') or self.w3c_no or ""
             conformance_level = values.get('conformance_level') or self.conformance_level or ""
-            description = values.get('description') or self.description or "" 
-            values['display_wcag_name'] = f"{w3c_no}{conformance_level}: {description}" 
+            description = values.get('description') or self.description or ""
+            values['display_wcag_name'] = f"{w3c_no}{conformance_level}: {description}"
         res = super(WcagRule, self).write(values)
         return res
-        
+
     @api.model
     def set_urls(self):
         for x in self.get_urls():
@@ -58,7 +59,7 @@ class WcagRule(models.Model):
                     yield (span[0].text.strip(), os.path.join(url, list_elem.get('href')))
         except Exception as e:
             _logger.warning(e)
-            
+
 # ~ #A class so we can have a unique wcag rule per projects, gets default values from a wcag.rule
 # ~ #############
 class WcagProjectRule(models.Model):
@@ -70,8 +71,8 @@ class WcagProjectRule(models.Model):
     w3c_no = fields.Char()
     conformance_level = fields.Char()
     description = fields.Char()
-    validate_method = fields.Selection([('auto', 'Automatic'),('manual', 'Manual')],'Validate Method', default='manual')
-    project_id = fields.Many2one(comodel_name = "project.project", string = "Project", help = "The project which this rule belongs to",ondelete='cascade') 
+    validate_method = fields.Selection(VALIDATION_SELECTIONS,'Validate Method', default='manual')
+    project_id = fields.Many2one(comodel_name = "project.project", string = "Project", help = "The project which this rule belongs to",ondelete='cascade')
 
     @api.model
     def create(self, vals):
@@ -86,29 +87,33 @@ class WcagProjectRule(models.Model):
         res = super(WcagProjectRule, self).create(vals)
 
         return res
-        
+
     def write(self, values):
         if values.get('description') or values.get('w3c_no') or values.get('conformance_level'):
             w3c_no = values.get('w3c_no') or self.w3c_no or ""
             conformance_level = values.get('conformance_level') or self.conformance_level or ""
-            description = values.get('description') or self.description or ""  
+            description = values.get('description') or self.description or ""
             values['display_wcag_name'] = f"{w3c_no}{conformance_level}: {description}"
-            
+
         res = super(WcagProjectRule, self).write(values)
         return res
-    
-    
+
+
 # ~ ##############
 
-    
+
 class ProjectTaskWcag(models.Model):
     _name = 'project.task.wcag'
     _rec_name = 'display_wcag_name'
     wcag_id = fields.Many2one(comodel_name = "wcag.rule", string = "Wcag Rule", readonly=True)
     wcag_project_id = fields.Many2one(comodel_name = "wcag.project.rule", string = "Wcag Project Rule", required=True)
-    validate_method = fields.Selection([('auto', 'Automatic'),('manual', 'Manual')],'Validate Method', default='manual')
+    validate_method = fields.Selection(VALIDATION_SELECTIONS,'Validate Method', default='manual')
     task_id = fields.Many2one(comodel_name = "project.task",string = "Task",ondelete='cascade')
-    wcag_state = fields.Selection([('1', 'Failed'),('2', '*blank*'),('3', 'Not relevant'), ('4', 'Partial'), ('5', 'OK')],'State', default='3', group_operator="max")
+    wcag_state = fields.Selection(
+            [('1', 'Failed'),('2', '*blank*'),('3', 'Not relevant'), ('4', 'Partial'), ('5', 'OK')],
+            'State',
+            default='3',
+            group_operator="max")
     notes = fields.Text(String = "Notes", group_operator="max")
     display_wcag_name = fields.Char()
 
@@ -117,16 +122,16 @@ class ProjectTaskWcag(models.Model):
             return self.task_id.project_id
         else:
             return False
-    
+
     task_project_id = fields.Many2one(comodel_name="project.project", default=get_project_id, readonly=True, string = "Project")
-    
+
     @api.model
     def create(self, vals):
         if vals.get('task_id'):
             vals['task_project_id'] = self.env['project.task'].browse(vals['task_id']).project_id.id
         else:
             vals['task_project_id'] = False
-            
+
         if vals.get('wcag_project_id'):
              vals['validate_method'] = self.env['wcag.project.rule'].browse(vals['wcag_project_id']).validate_method
              vals['wcag_id'] = self.env['wcag.project.rule'].browse(vals['wcag_project_id']).wcag_id.id
@@ -136,22 +141,22 @@ class ProjectTaskWcag(models.Model):
             vals['wcag_id'] = False
         res = super(ProjectTaskWcag, self).create(vals)
         return res
-        
+
     def write(self, values):
         if 'task_id' in values:
             if values.get('task_id'):
                 values['task_project_id'] = self.env['project.task'].browse(values['task_id']).project_id
             else:
                 values['task_project_id'] = False
-                
+
         if 'wcag_project_id' in values:
             values['wcag_id'] = self.env['wcag.project.rule'].browse(values['wcag_project_id']).wcag_id.id
             values['display_wcag_name'] = self.env['wcag.project.rule'].browse(values['wcag_project_id']).display_wcag_name
-            
+
         res = super(ProjectTaskWcag, self).write(values)
         return res
-        
-   
+
+
     def action_read_task_rule(self):
         self.ensure_one()
         return {
