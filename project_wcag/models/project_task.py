52f4bcce65525eee4e4315bb81d39667d765dcb5
Emanuel Bergsten <emanuel.bergsten@vertel.se>
2022-04-21 07:43:37 +0000

    Added way to parse AInspector report file (json)

--- project_wcag/models/project_task.py
+++ project_wcag/models/project_task.py
@@ -1,15 +1,62 @@
+import logging
+import base64
+import json
+import traceback
+
 from odoo import models, fields, _, api, SUPERUSER_ID
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
 
 from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
-    
+
+_logger = logging.getLogger(__name__)
+
 
 class ProjectTask(models.Model):
     _inherit = 'project.task'
-    task_wcag_ids = fields.One2many(comodel_name='project.task.wcag', string="Wcags", help="Is a list of wcag criteria for this task/object", inverse_name='task_id', ondelete='cascade') 
-    
+    task_wcag_ids = fields.One2many(comodel_name='project.task.wcag', string="Wcags", help="Is a list of wcag criteria for this task/object", inverse_name='task_id', ondelete='cascade')
+    ainspector_report = fields.Binary()
+
     rule_count = fields.Integer(compute='_compute_rule_count', string="Task Count")
     def _compute_rule_count(self):
         self.rule_count = len(self.task_wcag_ids) or 0
 
+
+    def add_ainspector_file(self, ainspector_value):
+        try:
+            file_data = json.loads(base64.b64decode(ainspector_value))
+            for wcag_rule in self.task_wcag_ids:
+                if wcag_rule.wcag_state:
+                    # Do not update if the state is already set.
+                    continue
+                results = []
+                for rule_result in file_data.get("rule_results", []):
+                    if rule_result.get('success_criteria_code') == wcag_rule.wcag_id.w3c_no:
+                        results.append(rule_result.get('result_value_nls'))
+                if 'V' in results:
+                    wcag_rule.wcag_state = '1'
+                elif 'MC' in results:
+                    wcag_rule.wcag_state = '5' # TODO: option for this
+                    wcag_rule.extra_notes = _("Set as OK due to MC rule")
+                elif results:
+                    wcag_rule.wcag_state = '5'
+
+            filename = datetime.now().strftime("Report_added_%Y%m%d%H%M%S.json")
+            attachment_vals = {
+                    'res_model': 'project.task',
+                    'res_id': self.id,
+                    'datas': ainspector_value,
+                    'name': filename,
+                    }
+            self.env['ir.attachment'].create(attachment_vals)
+        except:
+            _logger.warning(traceback.format_exc())
+            raise UserError(_("Unexpected file added to task."))
+
+    def write(self, vals):
+        if 'ainspector_report' in vals:
+            ainspector_value = vals.get('ainspector_report')
+            vals['ainspector_report'] = None
+            self.add_ainspector_file(ainspector_value)
+
+        return super(ProjectTask, self).write(vals)
