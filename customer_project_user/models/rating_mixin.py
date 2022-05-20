d548ff52780a3b0775cb0f19951d16e9dac1cc93
Miracle <miracleaayodele@gmail.com>
2022-05-20 14:38:10 +0000

    project customer user bug fiz

--- customer_project_user/models/rating_mixin.py
+++ customer_project_user/models/rating_mixin.py
@@ -11,3 +11,7 @@ class RatingMixin(models.AbstractModel):
     _inherit = 'rating.mixin'
 
     rating_ids = fields.One2many('rating.rating', 'res_id', string='Rating', groups='base.group_user,customer_project_user.group_project_customer_user', domain=lambda self: [('res_model', '=', self._name)], auto_join=True)
+
+    rating_last_value = fields.Float('Rating Last Value', groups='base.group_user,customer_project_user.group_project_customer_user', compute='_compute_rating_last_value', compute_sudo=True, store=True)
+    rating_last_feedback = fields.Text('Rating Last Feedback', groups='base.group_user,customer_project_user.group_project_customer_user', related='rating_ids.feedback')
+    rating_last_image = fields.Binary('Rating Last Image', groups='base.group_user,customer_project_user.group_project_customer_user', related='rating_ids.rating_image')
