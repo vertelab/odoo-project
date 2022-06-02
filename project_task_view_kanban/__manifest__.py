633b91a10624dc9af36865022d9113aa448aab89
Miracle <miracleaayodele@gmail.com>
2022-06-02 07:49:32 +0000

    show project tasks first

---
+++ project_task_view_kanban/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Enterprise Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "Project Task View Kanban",
+    'summary': 'Show Tasks first',
+    'author': 'Vertel AB',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-project.git',
+    'category': 'Project',
+    "version": "14.0.0.0.0",
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'license': 'AGPL-3',
+    'website': 'https://vertel.se/',
+    "description": """
+        Change Project sequence to show tasks first instead of project
+    """,
+    "depends": [
+        "project",
+    ],
+    "data": [
+        "views/menu.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
