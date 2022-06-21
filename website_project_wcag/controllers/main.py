e65534fbd0a9d30ec36ca065d15935f1ebc18094
Miracle <miracleaayodele@gmail.com>
2022-06-21 12:26:02 +0000

    wcag overview and details

--- website_project_wcag/controllers/main.py
+++ website_project_wcag/controllers/main.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from collections import OrderedDict
 from operator import itemgetter
+import re
 
 from odoo import fields, http, _
 from odoo.http import request
 from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 from odoo.addons.website.controllers.main import QueryURL
 from odoo.tools import groupby as groupbyelem
 
@@ -18,7 +18,7 @@ class ProjectWCAGeController(http.Controller):
     def is_wcag_projects(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
         values = CustomerPortal._prepare_portal_layout_values(self)
         Project = request.env['project.project'].sudo()
-        domain = []
+        domain = [('is_wcag', '=', True)]
 
         searchbar_sortings = {
             'date': {'label': _('Newest'), 'order': 'create_date desc'},
@@ -49,7 +49,7 @@ class ProjectWCAGeController(http.Controller):
         values.update({
             'date': date_begin,
             'date_end': date_end,
-            'projects': projects,
+            'wcag_projects': projects,
             'page_name': 'wcag_project',
             'default_url': '/wcag-projects',
             'pager': pager,
@@ -90,7 +90,7 @@ class ProjectWCAGeController(http.Controller):
         task_count = request.env['project.task'].sudo().search_count(domain)
         # pager
         pager = portal_pager(
-            url="/my/tasks",
+            url="/wcag-projects/<int:project_id>/tasks",
             url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby, 'filterby': filterby,
                       'groupby': groupby, 'search_in': search_in, 'search': search},
             total=task_count,
@@ -108,6 +108,7 @@ class ProjectWCAGeController(http.Controller):
             'date': date_begin,
             'date_end': date_end,
             'grouped_tasks': grouped_tasks,
+            'wcag_project': request.env['project.project'].sudo().browse(project_id),
             'page_name': 'wcag_task',
             'default_url': '/wcag-projects/<int:project_id>/tasks',
             'pager': pager,
@@ -136,7 +137,7 @@ class ProjectWCAGeController(http.Controller):
         if search and search_in:
             search_domain = []
             if search_in in ('content', 'all'):
-                search_domain = OR([search_domain, ['|', ('name', 'ilike', search), ('description', 'ilike', search)]])
+                search_domain = OR([search_domain, [('display_wcag_name', 'ilike', search)]])
             domain += search_domain
 
         # task count
@@ -160,7 +161,9 @@ class ProjectWCAGeController(http.Controller):
             'date': date_begin,
             'date_end': date_end,
             'wcags': wcag,
-            'page_name': 'wcags',
+            'wcag_project': request.env['project.task'].sudo().browse(task_id).project_id,
+            'wcag_task': request.env['project.task'].sudo().browse(task_id),
+            'page_name': 'task_wcags',
             'default_url': '/wcag-project-task/%s/wcags' % task_id,
             'pager': pager,
             'searchbar_inputs': searchbar_inputs,
@@ -171,3 +174,42 @@ class ProjectWCAGeController(http.Controller):
 
 
 
+    @http.route(['/wcag-projects/<int:project_id>/wcag-task-overview', '/wcag-projects/<int:project_id>/wcag-task-overview/page/<int:page>'],
+                type='http', auth="public", website=True)
+    def project_task_wcags_overview(self, page=1, project_id=None, date_begin=None, date_end=None, sortby=None, filterby=None, search=None, search_in='all', groupby=None, **kw):
+        values = CustomerPortal._prepare_portal_layout_values(self)
+
+        task_domain = [('project_id', '=', project_id)]
+
+        wcag_rule = request.env['wcag.rule'].sudo().search([], order='display_wcag_name asc')
+
+        tasks = request.env['project.task'].sudo().search(task_domain, order='name asc')
+        tasks_list = []
+
+        for task in tasks:
+            task_wcag_ids = task.mapped('task_wcag_ids').filtered(lambda r: r.wcag_id in wcag_rule).sorted(key=lambda r: r.wcag_id.display_wcag_name)
+            tasks_list.append(task_wcag_ids)
+
+        values.update({
+            'wcags': wcag_rule,
+            'tasks': tasks_list,
+        })
+
+
+        return request.render("website_project_wcag.project_task_wcag_overview", values)
+
+
+    
+    @http.route(['/wcag-project-task/<int:task_id>/wcag/<int:wcag_id>'], type='http', auth="public", website=True)
+    def view_project_task_wcag(self, task_id=None, wcag_id=None, **kw):
+        values = CustomerPortal._prepare_portal_layout_values(self)
+
+        wcag_domain = [('id', '=', wcag_id)]
+
+        wcag = request.env['project.task.wcag'].sudo().search(wcag_domain, limit=1)
+
+        values.update({
+            'wcag': wcag,
+            'page_name': 'task_wcag',
+        })
+        return request.render("website_project_wcag.wcag_details", values)
