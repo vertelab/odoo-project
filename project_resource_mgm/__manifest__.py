0eacb4933c35a70bd7aef005d7441e3e76d242e9
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-12 16:12:27 +0200

    Update __manifest__.py

--- project_resource_mgm/__manifest__.py
+++ project_resource_mgm/__manifest__.py
@@ -1,17 +1,40 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
-    'name': 'Project Resource Management',
-    'summary': 'Project Resource Management',
-    'contributor': 'Han Wong <han.wong@vertel.se>',
-    'repository': 'git@github.com:vertelab/odoo-project.git',
-    'category': 'Operations/Project',
+    'name': 'Project:  Resource Mgm',
     'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Project Resource Management.',
+    'category': 'Project',
+    'description': 'Project Resource Management.',
+    'sequence': '10',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-project/project_resource_mgm',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
-    'website': 'https://www.vertel.se',
+    'contributor': 'Han Wong <han.wong@vertel.se>',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-project',
     'description': 'Project Resource Management',
-    'sequence': 10,
     'depends': [
         'project',
         'hr',
@@ -27,3 +50,4 @@
     'auto_install': False,
     'application': True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
