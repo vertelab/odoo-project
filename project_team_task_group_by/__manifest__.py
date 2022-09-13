7b92981dc54c1fef2ced529873f10c7cdce3b538
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-13 16:18:11 +0200

    Update __manifest__.py

--- project_team_task_group_by/__manifest__.py
+++ project_team_task_group_by/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo SA, Open Source Enterprise Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,25 +20,32 @@
 ##############################################################################
 
 {
-    "name": "Project Team Task Group By",
+    'name': 'Project: Team Task Group By',
+    'version': '14.0.0.1.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adds a group by option for project teams on tasks',
-    'author': 'Vertel AB',
     'category': 'Project',
-    "version": "14.0.0.1.0",
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se/',
     "description": """
         'Adds a group by option for project teams on tasks'
     """,
-    "depends": [
-        "project",
-        "project_team",
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-project/project_team_task_group_by',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-project',
+    'depends': [
+        'project',
+        'project_team',
     ],
-    "data": [
-        "views/project_task_view.xml",
-        "views/project_view.xml",
+    'data': [
+        'views/project_task_view.xml',
+        'views/project_view.xml',
     ],
-    "demo": [],
-    "installable": True,
-    "application": False,
+    'demo': [],
+    'installable': True,
+    'application': False,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
