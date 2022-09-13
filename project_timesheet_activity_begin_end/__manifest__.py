9368af4ef25a672b64e5bd15faa9e41f12655f48
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-13 16:21:53 +0200

    Update __manifest__.py

--- project_timesheet_activity_begin_end/__manifest__.py
+++ project_timesheet_activity_begin_end/__manifest__.py
@@ -1,11 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': 'Project Task Timesheet Activities - Begin/End Hours',
+    'name': 'Project: Task Timesheet Activities - Begin/End Hours',
     'version': '14.0.1.0.3',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': '',
+    'category': 'Project',
+    'description': '',
+    #'sequence': '1',
     'author': 'Vertel AB',
-    'contributors': 'Verified Email Europe AB,Hemangi Rupareliya,Vertel AB,',
-    'website': 'https://vertel.se/',
+    'website': 'https://vertel.se/apps/odoo-project/project_timesheet_activity_begin_end',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
-    'category': 'Tools',
+    'contributor': 'Verified Email Europe AB, Hemangi Rupareliya, Vertel AB,',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-project',
     'depends': [
         'hr_timesheet_activity_begin_end' #https://github.com/OCA/timesheet/tree/14.0/hr_timesheet_activity_begin_end
     ],
@@ -15,3 +43,4 @@
     'application': False,
     'installable': True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
