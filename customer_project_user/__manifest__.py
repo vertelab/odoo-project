36c4e3ebd40e22d4ad0c4e5d2930e99b51c7c4f1
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-04 11:54:17 +0200

    Update __manifest__.py

--- customer_project_user/__manifest__.py
+++ customer_project_user/__manifest__.py
@@ -25,7 +25,9 @@
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'A user type for project customers.',
     'category': 'Project',
-    'description': """A user type for project customers""",
+    'description': """
+    A user type for project customers.
+    """,
     #'sequence': '1',
     'author': 'Vertel AB',
     'website': 'https://vertel.se/apps/odoo-project/customer_project_user',
