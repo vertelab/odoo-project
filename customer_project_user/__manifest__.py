a13b8d576a3ace4c604bf57c5327c58f5fa92350
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2023-02-02 12:11:44 +0000

    customer_project_user:removed dms rights

--- customer_project_user/__manifest__.py
+++ customer_project_user/__manifest__.py
@@ -36,7 +36,8 @@
     'contributor': '',
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-project',
-    'depends': ['project', 'base', 'hr_timesheet', 'dms', 'contacts', 'portal', 'web', 'project_scrum', 'calendar', 'mail', 'sale_project', 'sale_timesheet'],
+    # ~ 'depends': ['project', 'base', 'hr_timesheet', 'dms', 'contacts', 'portal', 'web', 'project_scrum', 'calendar', 'mail', 'sale_project', 'sale_timesheet'],
+    'depends': ['project', 'base', 'hr_timesheet', 'contacts', 'portal', 'web', 'project_scrum', 'calendar', 'mail', 'sale_project', 'sale_timesheet'],
     'data': [
         'security/groups.xml',
         'security/project_access.xml',
