9a7144d54ab88423771c0ccf17bf0aa4bc36140e
iammiracle <miracleaayodele@gmail.com>
2023-09-18 11:42:01 +0200

    change position of date_start field and date field on project_project and project_task form view

---
+++ vertel_project_timeline/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2016-2017 Tecnativa - Pedro M. Baeza
+# Copyright 2017 Tecnativa - Carlos Dauden
+# Copyright 2021 Open Source Integrators - Daniel Reis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Vertel Project Timeline",
+    "summary": "Vertel Timeline view for Projects",
+    "version": "16.0.1.1.0",
+    "category": "Project Management",
+    "website": "https://github.com/vertel/odoo-project",
+    "author": "Vertel AB",
+    "license": "AGPL-3",
+    "depends": ["project_timeline"],
+    "data": [
+        "views/project_project_view.xml",
+        "views/project_task_view.xml",
+    ],
+}
