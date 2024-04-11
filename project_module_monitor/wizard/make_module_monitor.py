f08593ac50163cf1c0d2e03c53c9ecf940424773
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-04-11 16:54:40 +0200

    Update make_module_monitor.py

--- project_module_monitor/wizard/make_module_monitor.py
+++ project_module_monitor/wizard/make_module_monitor.py
@@ -1,1 +1,7 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+import logging
+#import dateutil.relativedelta as relativedelta
+#from datetime import datetime
 
+_logger = logging.getLogger(__name__)
