39ef0995fab3af7140b0937600c3dfea58efe1c2
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-04-11 13:44:24 +0200

    Create __manifest__.py

---
+++ project_module_monitor/__manifest__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Project: Module Monitor',
+    'version': '14.0',
+    'summary': "We have the need to keep track of our modules",
+    'category': 'Technical',
+    'author': 'Vertel AB',
+    'website': "https://vertel.se/apps/odoo-project/project_module_monitor",
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-project',
+    'description': """
+   We need to keep track of the accurancy of our models.
+   What version do we have? 
+   What version do our valuable clients have?
+   How can we add good quality service by upgrading clients to a newer version of Odoo?
+       
+    """,
+    #'depends': ['project'],
+    'data': [
+   #   'views/res_config_settings_views.xml',
+   #   'views/task_view.xml',
+      ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
