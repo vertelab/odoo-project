81a848f4674eefa572287911ca00bdd4d767ce9a
Miracle Ayodele <miracleaayodele@gmail.com>
2024-04-24 11:30:03 +0200

    github sync repos

--- project_module_monitor/wizard/make_module_monitor.py
+++ project_module_monitor/wizard/make_module_monitor.py
@@ -14,37 +14,44 @@ GITHUB_RAW_URL  = 'https://raw.githubusercontent.com'
 
 _logger = logging.getLogger(__name__)
 
-  
+class GitRepo(models.TransientModel):
+      _name = 'git.repos'
+
+      name = fields.Char(string="Name")
+      author = fields.Char(string="Author")
+
+
 class make_module_monitor(models.TransientModel):
     _name = "project.module.monitor.wizard"
     _description = "Load moduels from GitHub."
 
     module_author = fields.Char(string="Authour") # example vertel,oca
-    
-    def _get_git_repo(self):
-        organization_repositories_url = f"{GITHUB_BASE_URL}/orgs/{self.module_author}/repos"
-        organization_repositories_url = f"{GITHUB_BASE_URL}/orgs/vertelab/repos"
-        response = requests.get(organization_repositories_url)
-        # ~ raise UserWarning("%s" % [(r['name'],r['full_name']) for r in response.json()])
-        if response.status_code == 200:
-            repositories = response.json()
-            return [(repo['name'],repo['name']) for repo in repositories]
-        else:
-            return [(None,None)]
-    git_repo = fields.Selection(_get_git_repo,string="Git Repo", help="For example odoo-l10n_se") # example l10n_se
-    project_id = fields.Many2one(comodel_name="project.project", default=lambda b: b.env.context.get('active_id'))
-    
 
-    @api.onchange('Xmodule_author')
-    def _change_module_autor(self):
-        organization_repositories_url = f"{GITHUB_BASE_URL}/orgs/{self.module_author}/repos"
-        response = requests.get(organization_repositories_url)
-        if response.status_code == 200:
-            repositories = response.json()
-            return [(repo['name'],repo['name']) for repo in repositories] 
-        else:
-            return [(None,None)]
-    
+    project_id = fields.Many2one(comodel_name="project.project", default=lambda b: b.env.context.get('active_id'))
+    git_repo = fields.Many2one('git.repos', string="Git Repos", domain="[('author', '=', module_author)]")
+
+    @api.onchange('module_author')
+    def _compute_git_repos(self):
+        for rec in self:
+            organization_repositories_url = f"{GITHUB_BASE_URL}/orgs/{rec.module_author}/repos?per_page=100"
+            while organization_repositories_url:
+                response = requests.get(organization_repositories_url)
+
+                if response.status_code == 200:
+                   repositories = response.json()
+                   _logger.info(f"{len(repositories)=}")
+               	   for repo in repositories:
+                       self._sync_git_repo(repo.get('name'), rec.module_author)
+                organization_repositories_url = response.links.get('next', {}).get('url', None)
+
+    def _sync_git_repo(self, repo_name, author):
+        repo_id = self.env['git.repos'].search([('name', '=', repo_name), ('author', '=', author)], limit=1)
+        if not repo_id:
+           repo_id = self.env['git.repos'].create({'name': repo_name, 'author': author})
+        return repo_id
+
+
+
     def load_modules(self):
         # ~ https://pygithub.readthedocs.io/en/latest/examples/Repository.html
         g = Github()
@@ -55,8 +62,7 @@ class make_module_monitor(models.TransientModel):
             # ~ raise UserWarning(f"Could not read {self.module_author}/{self.git_repo} {e}")
             repo = g.get_repo(f"self.module_author/odoo-l10n_se")
         branches = [b.name for b in repo.get_branches() if re.match("^\d*[.]0$",b.name)] 
-        
-        
+
         contents = repo.get_contents("")
 
         for content_file in contents:
@@ -69,7 +75,7 @@ class make_module_monitor(models.TransientModel):
                         module_branch[b] = eval(response.text)
                 if len(module_branch.keys())>0:
                     raise UserWarning("%s" % module_branch)
-                    # Create project.task 
+                    # Create project.task
                     # add list of branches
                     # Put it on the highest branch stage
                     # What info from manifest are we interersted in?
