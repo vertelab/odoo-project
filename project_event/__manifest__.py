86a4177b42591e21be69e90f1f4b83f8f8fa93b5
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-14 10:58:16 +0200

    Update __manifest__.py

--- project_event/__manifest__.py
+++ project_event/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Project: Flow between project and event',
-    'version': '16.0.0.0.1',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '0.1',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Setup a link for your online meeting.',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
