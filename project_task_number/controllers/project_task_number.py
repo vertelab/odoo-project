5c12de152ee084240360b4599288528f657934b8
marcuszxc <marcus.karlsson@vertel.se>
2025-02-19 15:30:38 +0000

    project_task_number: Fixed task/push (probably).

--- project_task_number/controllers/project_task_number.py
+++ project_task_number/controllers/project_task_number.py
@@ -30,7 +30,7 @@ class GitHubWebHooks(http.Controller):
         self.check_signature()
         git_dict = self.get_git_data()
         source_branch = git_dict.get("branch")
-        pattern = "^[0-9]+\.0$"
+        pattern = "^[0-9]+.0$"
         match = re.search(pattern, source_branch)
         if (not match or not match.group()) or "Do not p-file sync" in git_dict.get("message"):
             return {"status": "success", "message": "Webhook Ignored"}
@@ -73,7 +73,6 @@ class GitHubWebHooks(http.Controller):
             _logger.warning(f"The command '{' '.join(result.args)}' got this following error '{result.stderr if result.stderr else result.stdout}'")
         return result
 
-
     def check_git_login(self):
         if self.run(["git", "config", "--global", "user.email"]).stdout != "vertelbot@vertel.se":
             self.run(["git", "config", "--global", "user.email", "vertelbot@vertel.se"], capture_output=True, text=True)
@@ -100,7 +99,7 @@ class GitHubWebHooks(http.Controller):
         match = re.search(r'T/\d{4}', message)
         git_dict = {
             "match": re.search(r'T/\d{4}', payload_dict.get('head_commit',{}).get('message')),
-            "task_number": match.group() if match else "New",
+            "task_number": match.group() if match else False,
             "branch": payload_dict.get('ref','x/x/x').split('/')[2],
             "message": payload_dict.get('head_commit',{}).get('message'),
             "repo": payload_dict.get('repository',{}).get('name'),
@@ -122,12 +121,14 @@ class GitHubWebHooks(http.Controller):
                     ('email', '=', git_dict.get("committer_email")),  # Exact match
                     ('email_normalized', '=', git_dict.get("committer_email"))  # Match normalized email (if applicable)
                 ], limit=1)
+        _logger.error(f"{user=}")
         task = request.env['project.task'].sudo().search([('number','=',git_dict["task_number"])],limit=1)
+        _logger.error(f"{task=} {git_dict['task_number']=}")
         if not task:
             project = request.env['project.project'].sudo().search([('name','=',git_dict["repo"])],limit=1)
             if not project:
                 project = request.env['project.project'].sudo().create({'name': git_dict["repo"]})
-            task = request.env['project.task'].sudo().create({'project_id': project.id, 'name': git_dict.get("message"),'number': git_dict["task_number"],'user_id': user.id if user else None })
+            task = request.env['project.task'].sudo().create({'project_id': project.id, 'name': git_dict.get("message"),'number': git_dict["task_number"] if git_dict["task_number"] else _("New"),'user_id': user.id if user else None })
         if not project:
             project = task.project_id
         author_id= user.partner_id.id if user else request.env.user.partner_id.id
