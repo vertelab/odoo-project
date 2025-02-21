1358f3cb829b8af077000f36bb934533f427b409
marcuszxc <marcus.karlsson@vertel.se>
2025-02-21 14:31:59 +0000

    odoobranchpfile project_task_number from 16.0

---
+++ project_task_number/models/project_task.p.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError, AccessError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class Task(models.Model):
+    _inherit = "project.task"
+
+    number = fields.Char("Number", default=lambda self: _('New'),
+                     copy=False, readonly=True, tracking=True)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('number', _('New')) == _('New'):
+                vals['number'] = self.env['ir.sequence'].next_by_code('project.task.number') or _('New')
+        return super().create(vals_list)
+
+    def write(self, vals):
+        # ~ if 'number' not in vals:
+        if True:
+            for record in self:
+                if record.number == _('New'):
+                    vals['number'] = self.env['ir.sequence'].next_by_code('project.task.number') or _('New')
+        return super().write(vals)
