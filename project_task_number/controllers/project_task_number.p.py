58c208b432d6a3eed2813095de230403449460fa
marcuszxc <marcus.karlsson@vertel.se>
2025-02-24 10:42:53 +0000

    project_task_number: Now creates a sepret task for syncs if not task number is given

--- project_task_number/controllers/project_task_number.p.py
+++ project_task_number/controllers/project_task_number.p.py
@@ -14,6 +14,8 @@ import os
 
 _logger = logging.getLogger(__name__)
 
+p_file_sync = "Do not p-file sync"
+
 class GitHubWebHooks(http.Controller):
 
     @http.route(['/task/push'], type='json', auth="public", methods=["POST"], csrf=False)
@@ -23,7 +25,30 @@ class GitHubWebHooks(http.Controller):
         """
         self.check_signature()
         git_dict = self.get_git_data()
-        self.find_project(git_dict)
+        author_id=""
+        user = request.env['res.users'].sudo().search([
+                    '|',
+                    ('email', '=', git_dict.get("committer_email")),  # Exact match
+                    ('email_normalized', '=', git_dict.get("committer_email"))  # Match normalized email (if applicable)
+                ], limit=1)
+        _logger.error(f"{user=}")
+        res_partner_id = request.env['res.partner'].sudo().search([("email", "=", git_dict.get("committer_email"))], limit=1)
+        if res_partner_id and res_partner_id.name == "vertelbot":
+            author_id = res_partner_id
+        else:
+            author_id = user.partner_id.id if user else request.env.user.partner_id.id
+        task = self.find_task(git_dict)
+        _logger.error(f"{task=}")
+        project = self.find_project(git_dict,task)
+        _logger.error(f"{project=}")
+        if not task:
+            if p_file_sync in git_dict["message"]:
+                task = self.create_task(git_dict,user,project,"P-files Sync")
+            else:
+                task = self.create_task(git_dict,user,project)
+        message_id = self.send_task_message(git_dict,task,author_id)
+        _logger.warning(f'Success: {user=} {task=}  {project=} {git_dict.get("message")=} {git_dict["task_number"]=} {message_id.body=} {message_id.author_id.name=}')
+            
         return {"status": "success", "message": "Webhook processed successfully"}
 
     @http.route(['/sync/pfiles'], type='json', auth="public", methods=["POST"], csrf=False)
@@ -33,7 +58,7 @@ class GitHubWebHooks(http.Controller):
         source_branch = git_dict.get("branch")
         pattern = "^[0-9]+.0$"
         match = re.search(pattern, source_branch)
-        if (not match or not match.group()) or "Do not p-file sync" in git_dict.get("message"):
+        if (not match or not match.group()) or p_file_sync in git_dict.get("message"):
             return {"status": "success", "message": "Webhook Ignored"}
         files = self._get_files(git_dict)
         new_dir = str(uuid.uuid4())
@@ -52,7 +77,7 @@ class GitHubWebHooks(http.Controller):
             for file in files:
                 self.run(["git", "-C", f"{new_repo_path}", "checkout", f"{source_branch}", f"{file}"], capture_output=True, text=True)
             self.run(["git", "-C", f"{new_repo_path}", "add", "."], capture_output=True, text=True)
-            self.run(["git", "-C", f"{new_repo_path}", "commit", "-m", f"odoobranchpfile {git_dict.get('repo')} from {source_branch}. Do not p-file sync {git_dict.get('task_number', '')}"], capture_output=True, text=True)
+            self.run(["git", "-C", f"{new_repo_path}", "commit", "-m", f"odoobranchpfile {git_dict.get('repo')} from {source_branch}. {p_file_sync} {git_dict.get('task_number', '')}"], capture_output=True, text=True)
             self.run(["git", "-C", f"{new_repo_path}", "push"], capture_output=True, text=True)
 
         self.run(["rm", "-r", f"{new_path}"], capture_output=True, text=True)
@@ -118,39 +143,40 @@ class GitHubWebHooks(http.Controller):
         }
 
         return git_dict
+        
+    def create_task(self,git_dict,user,project,message=False):
+        # #if VERSION >= "15.0"
+        user_ids = [(6,0,[user.id])] if user else None
+        task_vals = {'project_id': project.id, 'name': git_dict.get("message"),'number': git_dict["task_number"] if git_dict["task_number"] else _("New"),'user_ids': user_ids }
+        # #elif VERSION <= "14.0"
+        task_vals = {'project_id': project.id, 'name': git_dict.get("message"),'number': git_dict["task_number"] if git_dict["task_number"] else _("New"),'user_id': user.id if user else None }
+        # #endif
+        if message:
+            task_vals.update({"name": message})
+        return  request.env['project.task'].sudo().create(task_vals)
+
+    def find_task(self,git_dict):
+        task = request.env['project.task'].sudo().search([('number','=',git_dict["task_number"])],limit=1)        
+        if not task and p_file_sync in git_dict["message"]:
+            task = request.env['project.task'].sudo().search([('name','=',"P-files Sync")],limit=1)            
+        return task
     
-    def find_project(self,git_dict):
-        project = None
-        user = request.env['res.users'].sudo().search([
-                    '|',
-                    ('email', '=', git_dict.get("committer_email")),  # Exact match
-                    ('email_normalized', '=', git_dict.get("committer_email"))  # Match normalized email (if applicable)
-                ], limit=1)
-        _logger.error(f"{user=}")
-        task = request.env['project.task'].sudo().search([('number','=',git_dict["task_number"])],limit=1)
-        _logger.error(f"{task=} {git_dict['task_number']=}")
-        if not task:
-            project = request.env['project.project'].sudo().search([('name','=',git_dict["repo"])],limit=1)
-            if not project:
-                project = request.env['project.project'].sudo().create({'name': git_dict["repo"]})
-            # #if VERSION >= "15.0"
-            user_ids = [(6,0,[user.id])] if user else None
-            task = request.env['project.task'].sudo().create({'project_id': project.id, 'name': git_dict.get("message"),'number': git_dict["task_number"] if git_dict["task_number"] else _("New"),'user_ids': user_ids })
-            # #elif VERSION <= "14.0"
-            task = request.env['project.task'].sudo().create({'project_id': project.id, 'name': git_dict.get("message"),'number': git_dict["task_number"] if git_dict["task_number"] else _("New"),'user_id': user.id if user else None })
-            # #endif
-        if not project:
+    def find_project(self,git_dict,task):
+        project = request.env['project.project'].sudo().search([('name','=',git_dict["repo"])],limit=1)        
+        if task:
             project = task.project_id
-        author_id= user.partner_id.id if user else request.env.user.partner_id.id
+        elif not project:
+            project = request.env['project.project'].sudo().create({'name': git_dict["repo"]})
+        return project
+
+    def send_task_message(self,git_dict,task,author_id):
         message_id = task.sudo().message_post(
             body=f'Github post {git_dict.get("message")} [Branch={git_dict["branch"]}] Repo={git_dict["repo"]}<br/>{git_dict.get("committer_name")} {git_dict.get("committer_email")}<br/>Added={git_dict.get("added")}<br/>Removed={git_dict.get("removed")}<br/>Modified={git_dict.get("modified")}<br/>{git_dict.get("url")}',
             author_id=author_id,  
-            # ~ message_type='comment',
             message_type='notification',
             subtype_xmlid='mail.mt_comment' 
         )
-        _logger.warning(f'Success: {user=} {task=}  {project=} {git_dict.get("message")=} {git_dict["task_number"]=} {message_id.body=} {message_id.author_id.name=}')
-
+        return message_id
 
     def _get_files(self,git_dict):
         strings_of_interest = [".p.", "index.html", ".png", ".jpg"]
