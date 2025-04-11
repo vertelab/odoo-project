d37d11c489c9e1b390afa1a1391f39d60fe9d893
vertelbot <vertelbot@vertel.se>
2025-04-11 07:30:50 +0000

    odoobranchpfile odoo-project from 14.0. Do not p-file sync T/1899

--- project_task_number/controllers/project_task_number.py
+++ project_task_number/controllers/project_task_number.py
@@ -95,7 +95,7 @@ class GitHubWebHooks(http.Controller):
         return {"status": "success", "message": "Webhook processed successfully"}
 
     def create_task(self,git_dict,user,project,message=False):
-        task_vals = {'project_id': project.id, 'name': git_dict.get("message"),'number': git_dict["task_number"] if git_dict["task_number"] else _("New"),'user_id': user.id if user else None }
+        task_vals = {'project_id': project.id, 'name': git_dict.get("message"),'number': git_dict["task_number"] if git_dict["task_number"] else _("New"),'user_id': user.id}
         if message:
             task_vals.update({"name": message})
         return  request.env['project.task'].sudo().create(task_vals)
@@ -118,7 +118,7 @@ class GitHubWebHooks(http.Controller):
         message_id = task.sudo().message_post(
             body=f'Github post {git_dict.get("message")} [Branch={git_dict["branch"]}] Repo={git_dict["repo"]}<br/>{git_dict.get("committer_name")} {git_dict.get("committer_email")}<br/>Added={git_dict.get("added")}<br/>Removed={git_dict.get("removed")}<br/>Modified={git_dict.get("modified")}<br/>{git_dict.get("url")}',
             author_id=author_id.id,  
-            message_type='notification',
+            message_type='comment',
             subtype_xmlid='mail.mt_comment' 
         )
         return message_id
