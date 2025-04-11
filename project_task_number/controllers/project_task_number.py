31979f9ee2f9dc62b7611044a84e654d930e6d3b
vertelbot <vertelbot@vertel.se>
2025-04-11 09:23:12 +0000

    odoobranchpfile odoo-project from 14.0. Do not p-file sync T/1870

--- project_task_number/controllers/project_task_number.py
+++ project_task_number/controllers/project_task_number.py
@@ -95,7 +95,15 @@ class GitHubWebHooks(http.Controller):
         return {"status": "success", "message": "Webhook processed successfully"}
 
     def create_task(self,git_dict,user,project,message=False):
-        task_vals = {'project_id': project.id, 'name': git_dict.get("message"),'number': git_dict["task_number"] if git_dict["task_number"] else _("New"),'user_id': user.id}
+        closing_stages = list(filter(lambda t: t.is_closed,project.type_ids))
+        stage = list(filter(lambda t: t.name == "Klar!" or t.name == "Done",closing_stages))
+        if not stage:
+            if closing_stages:
+                stage = closing_stages[0]
+        elif len(stage) > 1:
+            stage = stage[0]
+
+        task_vals = {'project_id': project.id, 'name': git_dict.get("message"),'number': git_dict["task_number"] if git_dict["task_number"] else _("New"),'user_id': user.id, "stage_id": stage.id if stage else False}
         if message:
             task_vals.update({"name": message})
         return  request.env['project.task'].sudo().create(task_vals)
