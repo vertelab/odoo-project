a0f1b7af46dd9bb58ffab0cd062a46568390ca62
marcuszxc <marcus.karlsson@vertel.se>
2025-04-28 11:15:26 +0000

    project_task_number: Fixed bug for sync messages in task-push and added description to queue job for sync

--- project_task_number/controllers/project_task_number.py
+++ project_task_number/controllers/project_task_number.py
@@ -59,6 +59,7 @@ class GitHubWebHooks(http.Controller):
             author_id = res_partner_id
         else:
             author_id = res_partner_id if res_partner_id else request.env.user.partner_id
+        return author_id
 
     @http.route(['/task/push'], type='json', auth="public", methods=["POST"], csrf=False)
     def task_push(self, **payload):
@@ -72,21 +73,23 @@ class GitHubWebHooks(http.Controller):
                     ('email', '=', git_dict.get("committer_email")),  # Exact match
                     ('email_normalized', '=', git_dict.get("committer_email"))  # Match normalized email (if applicable)
                 ], limit=1)
-        _logger.error(f"{user=}")
+        if not user:
+            user = request.env.ref("base.public_user")
         res_partner_id = request.env['res.partner'].sudo().search([("email", "=", git_dict.get("committer_email"))], limit=1)
         author_id=self.get_author_id(res_partner_id)
-        task = self.find_task(git_dict)
-        _logger.error(f"{task=}")
-        project = self.find_project(git_dict,task)
-        _logger.error(f"{project=}")
-        if not task:
-            if p_file_sync in git_dict["message"]:
-                task = self.create_task(git_dict,user,project,"P-files Sync")
-            else:
-                task = self.create_task(git_dict,user,project)
+        task = project = False
+        if git_dict.get("task_number"):
+            task,project = self.has_task_number(git_dict)
+        elif p_file_sync in git_dict["message"]:
+            task,project = self.is_sync_message(git_dict,user)
+        else:
+            project = self.find_project(git_dict)
+            task = self.create_task(git_dict,user,project)
+        if not project or not task:
+            _logger.error(f'Failure: {user=} {task=} {project=} {git_dict.get("message")=} {git_dict["task_number"]=} {message_id.body=} {message_id.author_id.name=}')
+            return {"status": "failure", "message": f"Webhook processed successfully, but {project=} or {task=} not set."}
         message_id = self.send_task_message(git_dict,task,author_id)
-        _logger.warning(f'Success: {user=} {task=}  {project=} {git_dict.get("message")=} {git_dict["task_number"]=} {message_id.body=} {message_id.author_id.name=}')
-            
+        _logger.warning(f'Success: {user=} {task=}  {project=} {git_dict.get("message")=} {git_dict["task_number"]=} {message_id.body=} {message_id.author_id.name=}')    
         return {"status": "success", "message": "Webhook processed successfully"}
 
     def create_task(self,git_dict,user,project,message=False):
@@ -95,18 +98,26 @@ class GitHubWebHooks(http.Controller):
             task_vals.update({"name": message})
         return  request.env['project.task'].sudo().create(task_vals)
 
-    def find_task(self,git_dict):
-        task = request.env['project.task'].sudo().search([('number','=',git_dict["task_number"])],limit=1)        
-        if not task and p_file_sync in git_dict["message"]:
-            task = request.env['project.task'].sudo().search([('name','=',"P-files Sync")],limit=1)            
-        return task
-    
-    def find_project(self,git_dict,task):
-        project = request.env['project.project'].sudo().search([('name','=',git_dict["repo"])],limit=1)        
+    def is_sync_message(self,git_dict,user):
+        project = self.find_project(git_dict)
+        task = request.env['project.task'].sudo().search([('name','=',"P-files Sync"),('project_id',"=",project.id)],limit=1)
+        if not task:
+            task = self.create_task(git_dict,user,project,"P-files Sync")
+        return task, project
+
+    def has_task_number(self,git_dict):
+        task = request.env['project.task'].sudo().search([('number','=',git_dict["task_number"])],limit=1)
+        project = self.find_project(git_dict,task)
+        return task, project
+
+    def find_project(self,git_dict,task=False):
+        project = request.env['project.project'].sudo().search([('name','=',git_dict["repo"])],limit=1)
         if task:
-            project = task.project_id
-        elif not project:
+            project = task.project_id           
+        
+        if not project:
             project = request.env['project.project'].sudo().create({'name': git_dict["repo"]})
+
         return project
 
     def send_task_message(self,git_dict,task,author_id):
@@ -143,5 +154,8 @@ class GitHubWebHooks(http.Controller):
         if (not match or not match.group()) or p_file_sync in git_dict.get("message") or not contains_p_files:
             return {"status": "success", "message": "Webhook Ignored"}
         project_sync_id = request.env["mail.channel"].sudo().create({"name": f"{uuid.uuid4()}","committer_email": git_dict.get("committer_email", "vertelbot@vertel.se")})
-        project_sync_id.with_delay().sync(git_dict,files)
+        _logger.error(f"{request.httprequest.headers=}")
+        _logger.error(f"{type(request.httprequest.headers)=}")
+        _logger.error(f"{dict(request.httprequest.headers)=}")
+        project_sync_id.with_delay(description=json.dumps(dict(request.httprequest.headers))).sync(git_dict,files)
         return {"status": "success", "message": "Webhook P-file sync processed successfully."}
