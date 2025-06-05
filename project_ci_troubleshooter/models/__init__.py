fbfcc6925af67f5bee8203a10a6e8d751488650d
marcuszxc <marcus.karlsson@vertel.se>
2025-06-05 15:00:21 +0000

    project_ci_troubleshooter: Created module not done!!!

---
+++ project_ci_troubleshooter/models/__init__.py
@@ -0,0 +1,2 @@
+from . import ai_quest
+from . import project_ci_branch_line
---
+++ project_ci_troubleshooter/models/project_ci_branch_line.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, AccessError, ValidationError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class ProjectCIBranchLine(models.Model):
+    _inherit = "project.ci.branch.line"
+    
+    def create_task(self):
+        action = super(ProjectCIBranchLine, self).create_task()
+        quest_id = self.env["ai.quest"].search([("ai_type", "=", "ci_troubleshooter")], limit=1)
+        task_id = action["res_id"]
+        quest_id.run(record=task_id, message=self.text)
+        
