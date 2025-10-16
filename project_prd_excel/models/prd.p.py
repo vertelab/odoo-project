cfb3fa78df474085a4d28177dd807d79d5887404
vertelbot <vertelbot@vertel.se>
2025-10-16 23:16:41 +0000

    odoobranchpfile odoo-project from 18.0. Do not p-file sync

--- project_prd_excel/models/prd.p.py
+++ project_prd_excel/models/prd.p.py
@@ -36,52 +36,45 @@ class ExcelWizard(models.TransientModel):
     file = fields.Binary(string="File", required=True)
  
  
-    field_keys = [
-        "csrd_id",
-        "csrd_esrs",
-        "csrd_dr",
-        "csrd_paragraph",
-        "csrd_related_ar",
-        "csrd_name",
-        "csrd_data_type",
-        "csrd_conditional_or_alternative_dp",
-        "csrd_may_v",
-        "csrd_appendix_b",
-        "csrd_appendix_c_less_then_750",
-        "csrd_appendix_c_more_then_750",
-        ]
 
     def import_excel(self):
 
             wb = openpyxl.load_workbook(filename=BytesIO(base64.b64decode(self.file)))
-
-            wslist = wb.sheetnames
-
-            wslist.pop(0)
-
-            for ws in wslist:
-
+            
+            # ~ raise UserError(f"{wb.sheetnames=}")
+            for ws in wb.sheetnames:
                 activews = wb[ws]
-                record = {}
+                record = {}                
+                for row in range(1, activews.max_row + 1):
+                    value = activews.cell(row=row,column=1).value
+                    if value and any(c.isdigit() for c in str(value)): # Has numbers
+                        record = {
+                            'page': ws,
+                            'no': activews.cell(row=row,column=1),
+                             'category':  activews.cell(row=row,column=2).value if len(activews.cell(row=row,column=2).value) < 25 else '',
+                             'name': activews.cell(row=row,column=2) if len(activews.cell(row=row,column=2).value) > 25 else activews.cell(row=row,column=3).value ,
+                            'prd_id': self.env.context['active_id'],
+                        
+                        }
+                        self.env['prd.requirement'].create(record)
 
-                for row in range(3, activews.max_row + 1):
-                    if activews.cell(row=row,column=1) != None:
-                        for col in range(1, 13):
-                            record_value = activews.cell(row=row, column=col).value
-                            if record_value != None:
-                                record_value = str(record_value).strip()
-                                record_value = record_value.lower()
-                                if record_value == "monerary":
-                                    record_value = "monetary"
-                                if "percentage" in record_value:
-                                    record_value = record_value.replace("percentage", "percent")
-                            record[self.field_keys[col-1]] = record_value
-                        record['csrd_sheet_name'] = ws
-                        self.create_record(record)
-            return {
-                'type': 'ir.actions.client',
-                'tag': 'reload',
-            }
+                    # ~ if activews.cell(row=row,column=1) != None:
+                        # ~ for col in range(1, 13):
+                            # ~ record_value = activews.cell(row=row, column=col).value
+                            # ~ if record_value != None:
+                                # ~ record_value = str(record_value).strip()
+                                # ~ record_value = record_value.lower()
+                                # ~ if record_value == "monerary":
+                                    # ~ record_value = "monetary"
+                                # ~ if "percentage" in record_value:
+                                    # ~ record_value = record_value.replace("percentage", "percent")
+                            # ~ record[self.field_keys[col-1]] = record_value
+                        # ~ record['csrd_sheet_name'] = ws
+                        # ~ self.create_record(record)
+            # ~ return {
+                # ~ 'type': 'ir.actions.client',
+                # ~ 'tag': 'reload',
+            # ~ }
 
 
     def create_record(self,record):
