4ad8c5071b20724d2a006f5dfc8ae3a383610f06
iammiracle <miracleaayodele@gmail.com>
2025-10-20 09:30:55 +0200

    project_scrum: updated mermaid with web_mermaid

--- project_task_add_default_stage/models/project_project.py
+++ project_task_add_default_stage/models/project_project.py
@@ -12,13 +12,14 @@ class ProjectTask(models.Model):
 
     use_default_types = fields.Boolean(string="Use default stages", default=True)
 
-    @api.model
-    def create(self, values):
-        res = super(ProjectTask, self).create(values)
-        if res.use_default_types:
-            default_types = self.env["project.task.type"].search(
-                [("is_default", "=", True)]
-            )
-            default_types_ids = default_types.mapped("id")
-            res.type_ids = [(4, x, 0) for x in default_types_ids]
+    @api.model_create_multi
+    def create(self, values_list):
+        res = super(ProjectTask, self).create(values_list)
+        for rec in res:
+            if rec.use_default_types:
+                default_types = self.env["project.task.type"].search(
+                    [("is_default", "=", True)]
+                )
+                default_types_ids = default_types.mapped("id")
+                rec.type_ids = [(4, x, 0) for x in default_types_ids]
         return res
