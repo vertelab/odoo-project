f7c627399eb9c9550d8f52d8a3b265ff3e54073f
vertelbot <vertelbot@vertel.se>
2025-11-11 09:18:00 +0000

    odoobranchpfile odoo-project from 18.0. Do not p-file sync T/6527

--- project_task_number/models/project_task.p.py
+++ project_task_number/models/project_task.p.py
@@ -30,7 +30,10 @@ class Task(models.Model):
     def create(self, vals_list):
         for vals in vals_list:
             if vals.get('number', _('New')) == _('New'):
-                vals['number'] = self.env['ir.sequence'].next_by_code('project.task.number') or _('New')
+                if not self.env.user._is_portal():
+                    vals['number'] = self.env['ir.sequence'].next_by_code('project.task.number') or _('New')
+                else:
+                    vals['number'] = self.env['ir.sequence'].sudo().next_by_code('project.task.number') or _('New')
         return super().create(vals_list)
 
     def write(self, vals):
