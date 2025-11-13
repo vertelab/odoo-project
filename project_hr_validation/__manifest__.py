e2f00f01bdc2a3b16dde79eb6400810666d8d858
iammiracle <miracleaayodele@gmail.com>
2025-11-13 09:49:46 +0100

    T/6515 project_hr_validation: adds manager as field who is the manager on the hr.department

---
+++ project_hr_validation/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project HR Validation",
+    "summary": """
+        Project HR Validation
+    """,
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/vertel/odoo-project",
+    "depends": ['hr'],
+    "data": [
+        'views/purchase_order_views.xml',
+    ],
+}
