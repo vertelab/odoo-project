ceda8f6414a952ef423f315c22adf5ce62bc857d
vertelbot <vertelbot@vertel.se>
2025-12-04 13:48:46 +0000

    odoobranchpfile odoo-project from 18.0. Do not p-file sync T/6746

--- project_task_number/models/project_task.p.py
+++ project_task_number/models/project_task.p.py
@@ -39,9 +39,13 @@ class Task(models.Model):
     def write(self, vals):
         if not vals:
             return True
-        if self.number == _('New'):
-            if not self.env.user._is_portal():
-                vals['number'] = self.env['ir.sequence'].next_by_code('project.task.number') or _('New')
-            else:
-                vals['number'] = self.env['ir.sequence'].sudo().next_by_code('project.task.number') or _('New')
-        return super().write(vals)
+        res = super().write(vals)
+        for record in self:
+            if record.number == _('New'):
+                if not record.env.user._is_portal():
+                    # ~ vals['number'] = self.env['ir.sequence'].next_by_code('project.task.number') or _('New')
+                    record.number = self.env['ir.sequence'].next_by_code('project.task.number') or False
+                else:
+                    # ~ vals['number'] = self.env['ir.sequence'].sudo().next_by_code('project.task.number') or _('New')
+                    record.number = self.env['ir.sequence'].sudo().next_by_code('project.task.number') or False
+        return res
