8d52230a70ff492510e0c96c33f033d79a30eb46
marcuszxc <marcus.karlsson@vertel.se>
2026-06-02 12:49:36 +0000

    changed how we filter projects for the new role

--- customer_project_user/models/__init__.py
+++ customer_project_user/models/__init__.py
@@ -1,5 +1,4 @@
 from . import project
 from . import res_users
 from . import ir_ui_menu
-from . import ir_rule
 
--- customer_project_user/models/project.py
+++ customer_project_user/models/project.py
@@ -1,10 +1,34 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import AccessError
+from odoo.osv import expression
 
 
 class ProjectProject(models.Model):
     _inherit = "project.project"
 
     customer_ids = fields.Many2many(comodel_name="res.partner", string="Customers")
+
+    def _get_customer_domain(self):
+        if self.env.user.has_group('customer_project_user.group_project_customer_user'):
+            return ['|', '|',
+                ('partner_id', '=', self.env.user.partner_id.id),
+                ('customer_ids', 'in', [self.env.user.partner_id.id]),
+                ('message_partner_ids', 'in', [self.env.user.partner_id.id])]
+        return []
+
+    @api.model
+    def _search(self, args, **kwargs):
+        domain = self._get_customer_domain()
+        if domain:
+            args = expression.AND([args, domain]) if args else domain
+        return super()._search(args, **kwargs)
+
+    def check_access_rule(self, operation):
+        super().check_access_rule(operation)
+        domain = self._get_customer_domain()
+        if domain:
+            if operation == 'read' and not self.filtered_domain(domain):
+                raise AccessError(_('You are not allowed to access this project.'))
 
     def write(self, vals):
         res = super().write(vals)
@@ -26,6 +50,28 @@ class ProjectProject(models.Model):
 
 class ProjectTask(models.Model):
     _inherit = "project.task"
+
+    def _get_customer_task_domain(self):
+        if self.env.user.has_group('customer_project_user.group_project_customer_user'):
+            return ['|', '|',
+                ('partner_id', '=', self.env.user.partner_id.id),
+                ('project_id.customer_ids', 'in', [self.env.user.partner_id.id]),
+                ('project_id.partner_id', '=', self.env.user.partner_id.id)]
+        return []
+
+    @api.model
+    def _search(self, args, **kwargs):
+        domain = self._get_customer_task_domain()
+        if domain:
+            args = expression.AND([args, domain]) if args else domain
+        return super()._search(args, **kwargs)
+
+    def check_access_rule(self, operation):
+        super().check_access_rule(operation)
+        domain = self._get_customer_task_domain()
+        if domain:
+            if operation == 'read' and not self.filtered_domain(domain):
+                raise AccessError(_('You are not allowed to access this task.'))
 
     @api.model_create_multi
     def create(self, vals_list):
