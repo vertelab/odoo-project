7456cff51a1a1f5ce913b1eb8810fda68ae09892
vertelbot <vertelbot@vertel.se>
2026-06-16 15:43:17 +0000

    odoobranchpfile odoo-project from 18.0. Do not p-file sync

--- project_task_number/__manifest__.py
+++ project_task_number/__manifest__.py
@@ -32,7 +32,7 @@
     'license': 'AGPL-3',
     'contributor': '',
     'maintainer': 'Vertel AB',
-    'depends': ['project', "queue_job"],
+    'depends': ['project'],
     'data': [
         'data/sequence.xml',
         'views/project_task_views.xml',
