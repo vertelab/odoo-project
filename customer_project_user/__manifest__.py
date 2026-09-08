69626abb397fd2f89248628d9e0de76ce8e6b431
Jakob Krabbe <jakob.krabbe@vertel.se>
2026-09-08 14:18:21 +0200

    [FIX] customer_project_user: re-add project_scrum to depends
    project_scrum was removed from depends in the Odoo-18 refactor commit,
    but security/groups.xml still references project_scrum models (e.g.
    project.project group inheritance). Since project_scrum is installed,
    re-adding it to depends ensures the models are loaded before
    customer_project_user's XML data.

--- customer_project_user/__manifest__.py
+++ customer_project_user/__manifest__.py
@@ -37,7 +37,7 @@
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-project',
     # ~ 'depends': ['project', 'base', 'hr_timesheet', 'dms', 'contacts', 'portal', 'web', 'project_scrum', 'calendar', 'mail', 'sale_project', 'sale_timesheet'],
-    'depends': ['project', 'base', 'hr_timesheet', 'mail', 'sale_project', 'sale_timesheet', 'web'],
+    'depends': ['project', 'base', 'hr_timesheet', 'mail', 'sale_project', 'sale_timesheet', 'web', 'project_scrum'],
     'data': [
         'security/groups.xml',
         'views/menu.xml',
